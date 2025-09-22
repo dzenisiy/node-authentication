73cfa5e3a7a0a36a7126047a92fd15d32a2b6e73
Yaraslau Dzenisiuk <yaraslau.dzenisiuk@dataart.com>
2025-09-22 15:11:51 +0200

    feat: add password reset

--- playground/server/routes/auth/resetpassword.js
+++ playground/server/routes/auth/resetpassword.js
@@ -36,7 +36,13 @@ module.exports = () => {
           /**
            * @todo: Find the user and create a reset token
            */
-          return next('Not implemented!');
+          const user = await UserService.findByEmail(req.body.email);
+          if (user) {
+            // eslint-disable-next-line no-unused-vars
+            const resetToken = await UserService.createPasswordResetToken(
+              user.id
+            );
+          }
         }
 
         if (errors.length) {
@@ -48,10 +54,14 @@ module.exports = () => {
           });
         }
 
+        req.session.messages.push({
+          text: 'If we found matching user you will recieve a link to reset',
+          type: 'info',
+        })
         /**
          * @todo: On success, redirect the user to some other page, like the login page
          */
-        return next('Not implemented!');
+        return res.redirect('/');
       } catch (err) {
         return next(err);
       }
@@ -66,7 +76,24 @@ module.exports = () => {
       /**
        * @todo: Validate the token and render the password change form if valid
        */
-      return next('Not implemented!');
+      const resetToken = await UserService.verifyPasswordResetToken(
+        req.params.userId,
+        req.params.resetToken
+      );
+
+      if (!resetToken) {
+        req.session.messages.push({
+          text: 'Token is invalid',
+          type: 'danger',
+        });
+        return res.redirect('/auth/resetpassword');
+      }
+
+      return res.render('auth/changepassword', {
+        page: 'resetpassword',
+        userId: req.params.userId,
+        resetToken: req.params.resetToken,
+      });
     } catch (err) {
       return next(err);
     }
@@ -81,6 +108,18 @@ module.exports = () => {
         /**
          * @todo: Validate the provided credentials
          */
+        const resetToken = await UserService.verifyPasswordResetToken(
+          req.params.userId,
+          req.params.resetToken
+        );
+
+        if (!resetToken) {
+          req.session.messages.push({
+            text: 'Token is invalid',
+            type: 'danger',
+          });
+          return res.redirect('/auth/resetpassword');
+        }
 
         const validationErrors = validation.validationResult(req);
         const errors = [];
@@ -108,7 +147,13 @@ module.exports = () => {
         /**
          * @todo: Change password, remove token and redirect to login
          */
-        return next('Not implemented!');
+        await UserService.changePassword(req.params.userId, req.body.password);
+        await UserService.deletePasswordResetToken(req.params.resetToken);
+        req.session.messages.push({
+          text: 'Password is changed',
+          type: 'success',
+        });
+        return res.redirect('/auth/login');
       } catch (err) {
         return next(err);
       }
